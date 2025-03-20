c3ff02abb43558e1d4f2d997636cf52f1635b8cd
Mr-cody-77 <sonu77mahata@gmail.com>
2025-03-20 22:50:52 +0530

    Adding winner time in model

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -18,7 +18,7 @@ class Player(models.Model):
 
 class Winner(models.Model):
     name = models.CharField(max_length=100)
-    time = models.FloatField() 
+    # time = models.FloatField() 
     def __str__(self):
         return f"{self.name}"
 
