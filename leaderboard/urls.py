90da43a768dd74fa0a232953542b00e8adc92fe6
Mr-cody-77 <sonu77mahata@gmail.com>
2025-03-20 15:25:31 +0530

    deploy fix 1

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import leaderboard_list, player_list, player_detail, winner_list
 
 urlpatterns = [
-     path("api/leaderboard/", leaderboard_list, name="leaderboard-list"),
-    path("api/player/", player_list, name="player-list"),  # /api/leaderboard/
-    path("api/winner/", winner_list, name="winner-list"),  # /api/leaderboard/
+    path("api/leaderboard/", leaderboard_list.as_view(), name="leaderboard-list"),
+    path("api/player/", player_list.as_view(), name="player-list"),
+    path("api/winner/", winner_list, name="winner-list"),
     path("api/player/<int:player_id>/", player_detail, name="player-detail"),
 ]
