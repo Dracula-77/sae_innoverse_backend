9828e9f1e82be83e40de88d3ad842e397fca9236
Mr-cody-77 <sonu77mahata@gmail.com>
2025-03-20 23:55:20 +0530

    leaderboard update

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Leaderboard(models.Model):
     name = models.CharField(max_length=100)
-    time = models.CharField(max_length=5) # Time taken for 10 questions (lower is better) # Default to an empty list
+    time = models.FloatField()  # Time taken for 10 questions (lower is better) # Default to an empty list
 
     def __str__(self):
         return f"{self.name} - {self.time}s"
